6cafe9ea3468cdcc37fb77cea9eb64273ad3f219
Kalimeris spyros <kalimerisspyros@gmail.com>
2025-02-27 15:34:45 +0200

    test

--- app/components/ShapesBackgound/ShapesBackground.tsx
+++ app/components/ShapesBackgound/ShapesBackground.tsx
@@ -4,14 +4,14 @@ import styles from "./ShapesBackground.module.css";
 const ShapesBackground = (): JSX.Element => {
   return (
     <div className={styles.container}>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
-      <div className={styles.shape}></div>
+      <div className={styles.shape} />
+      <div className={styles.shape} />
+      <div className={styles.shape} />
+      <div className={styles.shape} />
+      <div className={styles.shape} />
+      <div className={styles.shape} />
+      <div className={styles.shape} />
+      <div className={styles.shape} />
     </div>
   );
 };
